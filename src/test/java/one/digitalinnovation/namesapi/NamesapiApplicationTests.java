c33514bface29e083481f00344af7aa52591e91a
covesea <hermesansilva@gmail.com>
2021-06-25 17:07:11 -0400

    Pushing

---
+++ src/test/java/one/digitalinnovation/namesapi/NamesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.namesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
