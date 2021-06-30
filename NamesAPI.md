5bf075406d96eae67628d10d29f1020d64342bac
covesea <hermesansilva@gmail.com>
2021-06-29 20:16:21 -0400

    Changes

--- NamesAPI.md
+++
@@ -1,3 +0,0 @@
-#NamesAPI
-
-This is a project which I'm following from the DIO bootcamp to create a database of names and phone linked using Java.
--- src/main/java/one/digitalinnovation/namesapi/repository/NamesRepository.java
+++
@@ -1,7 +0,0 @@
-package one.digitalinnovation.namesapi.repository;
-
-import one.digitalinnovation.namesapi.entity.Names;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NamesRepository extends JpaRepository<Names, Long> {
-}
